c943b2a5ddffe25a16f1d7317aac15b074cab5d5
Till Niese <info@tn-dev.com>
2013-11-29 18:54:16 +0100

    formatting, new features and settings changes
    - fixed indent to spaces
    - added new helpers:
       • numberFormat
       • ifCond
    
    Settings changes:
    - changed default layout from 'layout.mustache' to 'main.mustache'
    - prefix layout templates with 'layouts' settings  instead of 'views'
    - passing 'bodyContent'  as 'SaveString'

--- handlebars-renderer.js
+++ handlebars-renderer.js
@@ -1,112 +1,165 @@
 var when = require('when'),
-    fs = require('fs');
+  fs = require('fs');
 
 exports.setup = function(options) {
-    options = options || {};
-    var Handlebars = options.Handlebars || require('handlebars');
+  options = options || {};
+  var Handlebars = options.Handlebars || require('handlebars');
 
-    //TODO add support for an asset module (prefixing of url)
+  //TODO add support for an asset module (prefixing of url)
 
-    Handlebars.registerHelper('appendAsset', function(url) {
-        this._assets = this._assets || [];
-        this._assets.push(url);
-        return "";
-    });
+  Handlebars.registerHelper('numberFormat', function(value, decimals, context) {
+    if (value !== undefined) {
+      var tmp = String(value).split(".");
+      if (!tmp[1]) {
+        tmp[1] = "00";
+      } else if (tmp[1].length == 1) {
+        tmp[1] += "0";
+      }
 
-    Handlebars.registerHelper('prependAsset', function(url) {
-        this._assets = this._assets || [];
-        this._assets.unshift(url);
-        return "";
-    });
+      return tmp.join(".");
+    } else {
+      return undefined;
+    }
+  });
 
-    Handlebars.registerHelper('includeAssets', function() {
-        this._assets = this._assets || [];
+  Handlebars.registerHelper("ifCond", function(v1, operator, v2, options) {
+    switch (operator) {
+      case "==":
+        return (v1 == v2) ? options.fn(this) : options.inverse(this);
 
-        var result = [];
+      case "!=":
+        return (v1 != v2) ? options.fn(this) : options.inverse(this);
 
-        var alreadyIncluded = {};
+      case "===":
+        return (v1 === v2) ? options.fn(this) : options.inverse(this);
 
-        this._assets.forEach(function(value) {
-            if (!alreadyIncluded[value]) {
-                if (value.match(/\.js$/)) {
-                    result.push('<script src="' + value + '" type="text/javascript" charset="utf-8"></script>');
-                } else {
-                    result.push('<link rel="stylesheet" href="' + value + '" type="text/css" media="screen" charset="utf-8">');
-                }
-                alreadyIncluded[value] = true;
-            }
-        });
+      case "!==":
+        return (v1 !== v2) ? options.fn(this) : options.inverse(this);
 
-        result = result.join("\n");
+      case "&&":
+        return (v1 && v2) ? options.fn(this) : options.inverse(this);
 
-        return new Handlebars.SafeString(result);
-    });
+      case "||":
+        return (v1 || v2) ? options.fn(this) : options.inverse(this);
 
-    render = function(path, opt, cb) {
+      case "<":
+        return (v1 < v2) ? options.fn(this) : options.inverse(this);
 
-        //TODO allow to define the default layout (via options)
-        //     and an alternative layout via 
-        when(getTemplates({
-            layout: opt.settings.views + "/layout.mustache",
-            content: path
-        })).then(function(tpls) {
-            try {
-                opt.bodyContent = tpls.content(opt);
+      case "<=":
+        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
 
-                var complete = tpls.layout(opt);
-                cb(null, complete);
-            } catch (e) {
-                cb(e);
-            }
-        });
-    };
+      case ">":
+        return (v1 > v2) ? options.fn(this) : options.inverse(this);
 
+      case ">=":
+        return (v1 >= v2) ? options.fn(this) : options.inverse(this);
 
-    function getTemplates(tpls) {
-        var d = when.defer();
+      default:
+        return eval("" + v1 + operator + v2) ? options.fn(this) : options.inverse(this);
+    }
+  });
 
-        var keys = [];
-        var loadList = [];
+  Handlebars.registerHelper('appendAsset', function(url) {
+    this._assets = this._assets || [];
+    this._assets.push(url);
+    return "";
+  });
 
-        for (var name in tpls) {
-            if (tpls.hasOwnProperty(name)) {
-                keys.push(name);
-                loadList.push(getTemplate(tpls[name]));
-            }
+  Handlebars.registerHelper('prependAsset', function(url) {
+    this._assets = this._assets || [];
+    this._assets.unshift(url);
+    return "";
+  });
+
+  Handlebars.registerHelper('includeAssets', function() {
+    this._assets = this._assets || [];
+
+    var result = [];
+
+    var alreadyIncluded = {};
+
+    this._assets.forEach(function(value) {
+      if (!alreadyIncluded[value]) {
+        if (value.match(/\.js$/)) {
+          result.push('<script src="' + value + '" type="text/javascript" charset="utf-8"></script>');
+        } else {
+          result.push('<link rel="stylesheet" href="' + value + '" type="text/css" media="screen" charset="utf-8">');
         }
+        alreadyIncluded[value] = true;
+      }
+    });
 
-        when.all(loadList)
-            .then(function(result) {
-            var list = {};
-            result.forEach(function(value, idx) {
-                list[keys[idx]] = value;
-            });
-            d.resolve(list);
-        });
+    result = result.join("\n");
 
-        return d.promise;
+    return new Handlebars.SafeString(result);
+  });
+
+  render = function(path, opt, cb) {
+
+    //TODO allow to define the default layout (via options)
+    //     and an alternative layout via 
+
+    when(getTemplates({
+      layout: opt.settings.layouts + "/main.mustache",
+      content: path
+    })).then(function(tpls) {
+      try {
+        opt.bodyContent = new Handlebars.SafeString(tpls.content(opt));
+
+        var complete = tpls.layout(opt);
+        cb(null, complete);
+      } catch (e) {
+        cb(e);
+      }
+    });
+  };
+
+
+  function getTemplates(tpls) {
+    var d = when.defer();
+
+    var keys = [];
+    var loadList = [];
+
+    for (var name in tpls) {
+      if (tpls.hasOwnProperty(name)) {
+        keys.push(name);
+        loadList.push(getTemplate(tpls[name]));
+      }
     }
 
-
-    function getTemplate(path) {
-        var d = when.defer();
-
-        fs.readFile(path,
-
-        function(err, data) {
-            if (err) throw err;
-            var template = Handlebars.compile(String(data));
-            d.resolve(template);
+    when.all(loadList)
+      .then(function(result) {
+        var list = {};
+        result.forEach(function(value, idx) {
+          list[keys[idx]] = value;
         });
+        d.resolve(list);
+      });
 
-        return d.promise;
-    }
+    return d.promise;
+  }
+
+
+  function getTemplate(path) {
+    var d = when.defer();
+
+    fs.readFile(path,
+
+      function(err, data) {
+        if (err) throw err;
+        var template = Handlebars.compile(String(data));
+        d.resolve(template);
+      });
+
+    return d.promise;
+  }
 };
 
 var render = function() {
-    throw new Error("renderer was not setup call 'setup()'");
+  throw new Error("renderer was not setup call 'setup()'");
 };
 
 exports.__express = function() {
-    render.apply(this, arguments);
+  render.apply(this, arguments);
 };
